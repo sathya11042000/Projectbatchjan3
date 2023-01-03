8b28d25570a374ce064bc4678d42d94f335e6462
sathya11042000 <sathyastudy2000@gmail.com>
2023-01-03 12:24:25 +0530

    Banu added a code

--- src/org/sample/Sample.java
+++ src/org/sample/Sample.java
@@ -1,6 +1,11 @@
 package org.sample;
 
 public class Sample {
+	private void m2() {
+		System.out.println("banu added her code");
+
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("main method");
 	}
