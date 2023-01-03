b6a8eed9b2bb5e3cd9c73a82e658555129713545
sathya11042000 <sathyastudy2000@gmail.com>
2023-01-03 12:07:55 +0530

    Abi done her work

--- src/org/sample/Sample.java
+++ src/org/sample/Sample.java
@@ -1,6 +1,10 @@
 package org.sample;
 
 public class Sample {
+	private void m1() {
+		System.out.println("abi code added");
+
+	}
 	public static void main(String[] args) {
 		System.out.println("main method");
 	}
